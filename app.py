7f4c80aef01d6ab6848f4cba61441963a7cf4d10
조윤서 <124692313+chobbong@users.noreply.github.com>
2023-08-18 21:52:31 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import streamlit as st
 
 
-# # Initialize connection.
-# conn = st.experimental_connection('mysql', type='sql')
+# Initialize connection.
+conn = st.experimental_connection('mysql', type='sql')
 
-# # Perform query.
-# df = conn.query("SELECT `매매대금_평균` FROM estate_data WHERE 시군구='충청북도 충주시 호암동-221' AND 단지명='호암리버빌(1단지)';", ttl=600)
+# Perform query.
+df = conn.query("SELECT `매매대금_평균` FROM estate_data WHERE 시군구='충청북도 충주시 호암동-221' AND 단지명='호암리버빌(1단지)';", ttl=600)
 
 
-# # Print results.
-# for row in df.itertuples():
-#     st.write(row)
+# Print results.
+for row in df.itertuples():
+    st.write(row)
 
 st.write("Hello world!")
